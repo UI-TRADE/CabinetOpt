7c23c38a6faa3a3893dc379d7e428bbd55fd387c
refact product sizes, repr and ranges added <ya1814@yandex.ru>
2024-02-09 10:54:39 +0300

    add verbose name to apps

--- settings_and_conditions/apps.py
+++ settings_and_conditions/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class SettingsAndConditionsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'settings_and_conditions'
+    verbose_name = 'Настройки и условия работы'
