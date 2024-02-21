a1440d17559b2f8df4cf6ef573bcf6121e071294
refact product sizes, repr and ranges added <ya1814@yandex.ru>
2024-02-21 21:51:00 +0300

    fix the code format

--- catalog/views.py
+++ catalog/views.py
@@ -32,7 +32,6 @@ from .tasks import (
     run_uploading_stock_and_costs
 )
 
-
 def parse_filters(filters):
     result = collections.defaultdict(list)
     range_filters = {}
@@ -111,18 +110,6 @@ class ProductView(FiltersView, ListView):
         return products, json.dumps({key: value.__json__() for key, value in filters.items()})
 
     def get_context_data(self, *, object_list=None, **kwargs):
-        self.object_list = Product.objects.none()
-        context = {
-            'products': self.object_list,
-            'filters': {}, 'is_sized': False,
-            'MEDIA_URL': settings.MEDIA_URL
-        }
-        ''' 
-        Для get запроса возвращаем пустой набор,
-        ибо получение данных реализованно только на POST
-        '''
-        if self.request.method == 'GET':
-            return context
         self.object_list, filters = self.get_queryset()
         paginator = Paginator(self.object_list, self.paginate_by)
         page = self.request.GET.get('page')
@@ -134,11 +121,11 @@ class ProductView(FiltersView, ListView):
         except EmptyPage:
             products_page = paginator.page(paginator.num_pages)
 
-        # context = super().get_context_data(**kwargs)
-        # context['MEDIA_URL']   = settings.MEDIA_URL
+        context = super().get_context_data(**kwargs)
         context['products']    = products_page
         context['filters']     = filters
         context['is_sized']    = StockAndCost.objects.filter(product__in=products_page, size__isnull=False).values_list('product_id', flat=True)
+        context['MEDIA_URL']   = settings.MEDIA_URL
         return context
 
 
