463cd00b735651b2cf5418696d90ab373df2a3c0
refact product sizes, repr and ranges added <ya1814@yandex.ru>
2024-05-03 17:59:50 +0300

    fix bug with images uploading

--- catalog/tasks.py
+++ catalog/tasks.py
@@ -273,7 +273,7 @@ def run_uploading_images(uploading_images):
                     defaults={
                         'product': product,
                         'filename': item['filename'],
-                        'order': item.get('item', 1),
+                        'order': item.get('order', 1),
                         'image': image
                 })
 
