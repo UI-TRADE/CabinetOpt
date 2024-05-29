3838d3eb1a0b3b1b17573cb2c55ddfb9c12dcc76
refact product sizes, repr and ranges added <ya1814@yandex.ru>
2024-05-29 20:21:57 +0300

    turn off inn validation

--- clients/forms.py
+++ clients/forms.py
@@ -74,10 +74,12 @@ class RegForm(forms.ModelForm):
 
     def clean_identification_number(self):
         value = self.cleaned_data['identification_number']
-        if not (len(value) == 10 or len(value) == 12):
+        if not (len(value) <= 12):
             raise ValidationError('Неверное количество символов ИНН')
-        if not self.inn_is_valid(value):
-            raise ValidationError('Контрольное число ИНН не совпадает с расчитанным')  
+        # if not (len(value) == 10 or len(value) == 12):
+        #     raise ValidationError('Неверное количество символов ИНН')
+        # if not self.inn_is_valid(value):
+        #     raise ValidationError('Контрольное число ИНН не совпадает с расчитанным')  
         if RegistrationOrder.objects.filter(identification_number=value).exists():
             raise ValidationError('Заявка на регистрацию с таким ИНН уже существует')
         return value
