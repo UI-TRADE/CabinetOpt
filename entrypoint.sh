120409ab8752a46509cedf902f5f97c97783426c
refact product sizes, repr and ranges added <ya1814@yandex.ru>
2024-07-10 13:48:56 +0300

    run rq worker in stage

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,6 +16,6 @@ fi
 python manage.py flush --no-input
 python manage.py migrate
 python manage.py createsuperuser --noinput --username $DB_USER --email $DB_EMAIL
-python manage.py mailing & python manage.py runserver 0.0.0.0:8000
+python manage.py rqworker default --with-scheduler & python manage.py runserver 0.0.0.0:8000
 
 exec "$@"
