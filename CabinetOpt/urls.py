6b03a5fdc759542425946c97e947c71a65e2cdc6
refact product sizes, repr and ranges added <ya1814@yandex.ru>
2024-08-22 20:43:39 +0300

    add silk url for prod

--- CabinetOpt/urls.py
+++ CabinetOpt/urls.py
@@ -36,11 +36,11 @@ urlpatterns = [
 
 urlpatterns += [
     path('captcha/', include('captcha.urls')),
+    path('silk/', include('silk.urls', namespace='silk')),
 ]
 
 if settings.DEBUG:
     urlpatterns += [
-        path('silk/', include('silk.urls', namespace='silk')),
         path('__debug__/' , include('debug_toolbar.urls')),
     ]
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
